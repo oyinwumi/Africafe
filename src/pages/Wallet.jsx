3122b1afa18bf6f3dbe0a7aef97f10d9bc6b1fbc
“Oyindamola” <oyinwumi0229@gmail.com>
2023-09-15 14:15:14 +0100

    update two

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -3,7 +3,7 @@ import { useLocation } from "react-router"
 import Navbar from "../components/Navbar"
 import Square from "../assets/square.svg";
 import Info from "../assets/info.svg";
-import { info } from "autoprefixer";
+import Circle from "../assets/circle.svg";
 
 
 const Wallet = () => {
@@ -40,23 +40,51 @@ const Wallet = () => {
             <p>See all activities on your account</p>
            </div>
            <div className="bg-[#D9D9D9] w-[1123px] h-[723px] mx-auto boreder border-[#D9D9D] mb-20">
-              <div className="flex w-full p-4">
+              <div className="flex gap-10 w-full p-6">
                   {
                     walletData.map((item, key)=>{
-                      return (<div key={key} className=" bg-[#FFFFFF] border border-[#FFFFFF] rounded-none w- gap-10">
+                      return (<div key={key} className=" bg-[#FFFFFF] border border-[#FFFFFF] rounded-none w-1/2 p-5">
                         <div className="flex justify-between">
                           <div className="flex gap-4">
-                          <img src={Square} alt="" /><span>{item.title}</span>
+                          <img src={Square} alt="" /><span className="text-[18px]">{item.title}</span>
                           </div>
                           <img src={Info} alt="" />
                         </div>
-                        <div>
-                          <p>{item.amount}</p>
-                          <button>{item.action}</button>
+                        <div className="flex justify-between mt-10">
+                          <p className="text-[24px]">{item.amount}</p>
+                          <button className="bg-[#D9D9D9] p-2 text-center rounded text-[14px]">{item.action}</button>
                         </div>
                       </div>)
                     })
                   }
+              </div>
+              <div className="p-6  mt-10">
+                 <table className="w-full">
+                    <thead>
+                      <tr className="flex justify-between my-6">
+                      <th>Recent Activities</th>
+                        <th>View all</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {
+                        recentActivities.map((activity, key)=>{
+                          return <tr key={key} className="flex justify-between "> 
+                             <div className="flex gap-2 items-center">
+                              <img src={Circle} alt="" />
+                              <div className="flex flex-row gap-2" >
+                              <td >{activity.action}</td>
+                              <td >{activity.date}</td>
+                              </div>
+                              
+                             </div>
+                             
+                             <td>{activity.amount}</td>
+                          </tr>
+                        })
+                      }
+                    </tbody>
+                 </table>
               </div>
            </div>
         </div>
